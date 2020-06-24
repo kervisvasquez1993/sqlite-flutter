880336896d6d201b72806e1ddec799f17d16f6f0
kervisvasquez1993 <kervisvasquez24@gmail.com>
2020-06-24 17:02:10 -0400

    agregamos theme a nuestro app

--- lib/src/page/HomePage.dart
+++ lib/src/page/HomePage.dart
@@ -12,8 +12,26 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        title: Text('Scaner Lector'),
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.delete_forever),
+            onPressed: (){
+
+            },
+          )
+        ],
+
+      ),
       body: _callPage(currentIndex),
-        bottomNavigationBar: _crearBottomNavigationBar(),
+      bottomNavigationBar: _crearBottomNavigationBar(),
+       floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked ,
+      floatingActionButton: FloatingActionButton(
+        child: Icon( Icons.filter_center_focus),
+        onPressed: (){},
+        backgroundColor: Theme.of(context).primaryColor,
+      ),
             );
           }
 
