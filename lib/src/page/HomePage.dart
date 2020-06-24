f378f9464d0cffb266a0c619094c40ad89260a4e
kervisvasquez1993 <kervisvasquez24@gmail.com>
2020-06-24 16:33:56 -0400

    agregamos menu y staful widget

--- lib/src/page/HomePage.dart
+++ lib/src/page/HomePage.dart
@@ -1,24 +1,53 @@
 import 'package:flutter/material.dart';
-class HomePage extends StatelessWidget {
+import 'package:sqlite/src/page/direcciones_page.dart';
+import 'package:sqlite/src/page/mapas_dart.dart';
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+
+  int currentIndex = 1;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text('hola home page!')),
+      body: _callPage(currentIndex),
         bottomNavigationBar: _crearBottomNavigationBar(),
             );
           }
 
   _crearBottomNavigationBar() {
     return BottomNavigationBar(
-      currentIndex: 0,
-      onTap: (index){},
+      currentIndex: currentIndex,
+      onTap: (index){
+        setState(() {
+          currentIndex = index;
+        });
+      },
       items: [
-        botoo
+        BottomNavigationBarItem(
+          icon: Icon(Icons.map),
+          title: Text('Mapas')
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(Icons.brightness_5),
+          title: Text('Direcciones')
+        ),
 
       ],
     );
   }
-        
-          
+
+
+  
+  Widget _callPage(int PaginaActual) {
+    switch( PaginaActual ) {
+      case 0 : return MapasPage();
+      case 1 : return DireccionPage();
+
+      default:
+        return MapasPage();
+    }
+  }
 }
